c5d02d8a15da2713bb11a989e4a5512ebc164b46
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-16 17:27:42 -0500

    Backend - Add dependency

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import MenuRoutes from "./src/routes/menu.routes.js";
 
 const servidor = express();
 
+servidor.use(cors());
 servidor.use(bodyParser.json());
 servidor.use(bodyParser.urlencoded({ extended: false }));
+
 servidor.use("/api", MenuRoutes);
 
 servidor.listen(3000, () => {
