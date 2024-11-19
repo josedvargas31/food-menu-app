6b1917ec1c8d2cd1c071c22f92ad01564362de22
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-19 17:33:24 -0500

    Frontend - Add folders & view

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1 className="text-2xl underline text-slate-500"> hola, soy el header...</h1>
+
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ frontend/src/components/pages/Menu.tsx
@@ -0,0 +1,15 @@
+import Header from '../Header'
+
+
+function Menu() {
+    return (
+        <>
+            <div className="">
+                <Header />
+                <h1 className=""> Hola, soy la vista que tendra todo</h1>
+            </div>
+        </>
+    )
+}
+
+export default Menu
