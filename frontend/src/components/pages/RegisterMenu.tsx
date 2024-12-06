8cd81661c9f4d615a4f362dd2beb70b03be43e60
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-12-06 14:46:07 -0500

    Frontend - Add register form

--- frontend/src/components/pages/RegisterMenu.tsx
+++ frontend/src/components/pages/RegisterMenu.tsx
@@ -1,32 +1,33 @@
-import { useRef } from 'react'
-// import axiosClient from '../client/axiosClient'
+import React, { useRef } from 'react'
+import axiosClient from '../client/axiosClient'
 
+type FormElement = React.FormEvent<HTMLFormElement>
 function RegisterMenu() {
-  const menu = useRef(null)
-  const bebida = useRef(null)
-  const dia = useRef(null)
+  const menu = useRef<HTMLInputElement>(null)
+  const bebida = useRef<HTMLInputElement>(null)
+  const dia = useRef<HTMLSelectElement>(null)
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault()
-  //   const data = {
-  //     menu: menu.current.value,
-  //     bebida: bebida.current.value,
-  //     dia: dia.current.value
-  //   }
-  //   axiosClient.post("/api/registrar", data).then((response)) => {
-  //     if (response.status == 200) {
-  //       alert("Menu registrado con exito")
-  //       window.location.reload()
-  //     } else {
-  //       alert("No se pudo registrar el menu, intente mas tarde")
-  //     }
-  //   }
-  // }
+  const handleSubmit = async (e: FormElement) => {
+    e.preventDefault()
+    const data = {
+      menu: menu.current?.value,
+      bebida: bebida.current?.value,
+      dia: dia.current?.value
+    }
+    axiosClient.post("/api/registrar", data).then((response) => {
+      if (response.status == 200) {
+        alert("Menu registrado con exito")
+        window.location.reload()
+      } else {
+        alert("No se pudo registrar el menu, intente mas tarde")
+      }
+    })
+  }
 
   return (
     <>
       <h1 className='text-8xl flex justify-center items-center'>Hola, soy la vista de registro</h1>
-      <form /* onSubmit={} */>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Ingrese el menu" className="" ref={menu} />
         <input type="text" placeholder="Ingrese bebida" className="" ref={bebida} />
         <select name="" id="" className="" ref={dia}>
